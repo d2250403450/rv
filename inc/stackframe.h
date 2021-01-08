aec932b03b504ef197e8eacc30c39eb82bde77d7
d2250403450 <2250403450@qq.com>
2021-01-08 16:37:57 +0800

    5 commit

--- inc/stackframe.h
+++ inc/stackframe.h
@@ -3,26 +3,33 @@
 #include <asm/asm.h>
 #include <trap.h>
 
+
+/*
+* mfc0 rt, rd 将CP0中的rd寄存器内容传输到rt通用寄存器；
+* mtc0 rt, rd 将rt通用寄存器中内容传输到CP0中寄存器rd；
+*/
+
+
 .macro STI
-	mfc0	t0,	CP0_STATUS
+	mfc0	t0,	CP0_STATUS	//把处理器状态与控制寄存器中的内容存入t0通用寄存器中
 	li		t1, (STATUS_CU0 | 0x1)
 	or		t0, t0, t1
-	mtc0	t0, CP0_STATUS
+	mtc0	t0, CP0_STATUS	//把t0通用寄存器中的内容赋给处理器状态与控制寄存器
 	
 .endm
 
 
 .macro CLI
-	mfc0	t0, CP0_STATUS
+	mfc0	t0, CP0_STATUS	//把处理器状态与控制寄存器中的内容存入t0通用寄存器中
 	li		t1, (STATUS_CU0 | 0x1)
 	or		t0, t0, t1
 	xori	t0, t0, 0x1
-	mtc0	t0, CP0_STATUS
+	mtc0	t0, CP0_STATUS	//把t0通用寄存器中的内容赋给处理器状态与控制寄存器
 .endm
 
 .macro SAVE_ALL    
                                   
-		mfc0	k0,CP0_STATUS                   
+		mfc0	k0,CP0_STATUS     //把处理器状态与控制寄存器中的内容存入t0通用寄存器中             
 		slli	k0, k0, 3      /* extract cu0 bit */  
 		bltz	k0, 1f                            
 		nop       
@@ -39,17 +46,17 @@
 		sub		sp,k1,TF_SIZE                    
 		sw		k0,TF_REG29(sp)                  
 		sw		x2,TF_REG2(sp)                   
-		mfc0	v0,CP0_STATUS                    
+		mfc0	v0,CP0_STATUS		//把处理器状态与控制寄存器中的内容存入v0通用寄存器中                    
 		sw		v0,TF_STATUS(sp)                 
-		mfc0	v0,CP0_CAUSE                     
+		mfc0	v0,CP0_CAUSE       	//把最近异常原因寄存器中的内容存入v0通用寄存器中              
 		sw		v0,TF_CAUSE(sp)                  
-		mfc0	v0,CP0_EPC                       
+		mfc0	v0,CP0_EPC         	//把EJTAG调试异常时最近的程序计数器值存入v0通用寄存器            
 		sw		v0,TF_EPC(sp)
-		mfc0	v0, CP0_BADVADDR        
+		mfc0	v0, CP0_BADVADDR    //把最近异常地址寄存器的内容存器v0通用寄存器  
 		sw		v0, TF_BADVADDR(sp)            
-		mfhi	v0                               
+		mfhi	v0                  //把hi寄存器的内容存入v0寄存器（保存乘法除法结果的寄存器，用于恢复现场）           
 		sw		v0,TF_HI(sp)                     
-		mflo	v0                               
+		mflo	v0                  //把lo寄存器的内容存入v0寄存器（保存乘法除法结果的寄存器，用于恢复现场）            
 		sw		v0,TF_LO(sp)                     
 		sw		x0,TF_REG0(sp)
 		sw		x1,TF_REG1(sp)                    
@@ -89,23 +96,23 @@
  */
 .macro RESTORE_SOME                                      
 		// .set	mips1                            
-		mfc0	t0,CP0_STATUS                    
+		mfc0	t0,CP0_STATUS	//把处理器状态与控制寄存器的内容赋给t0通用寄存器                    
 		ori		t0, t0, 0x3                          
 		xori	t0, t0, 0x3                          
-		mtc0	t0,CP0_STATUS                    
+		mtc0	t0,CP0_STATUS	//把t0通用寄存器的内容赋给处理器状态与控制寄存器                    
 		lw		v0,TF_STATUS(sp)             
 		li		v1, 0xff00 				 
 		and		t0, t0, v1 					 
 		nor		v1, x0, v1 				 
 		and		v0, v0, v1 					 
 		or		v0, v0, t0 					 
-		mtc0	v0,CP0_STATUS 		 
+		mtc0	v0,CP0_STATUS	//把t0通用寄存器的内容赋给处理器状态与控制寄存器		 
 		lw		v1,TF_LO(sp)                                       
-		mtlo	v1                               
+		mtlo	v1              //把lo寄存器的内容存入v1寄存器                  
 		lw		v0,TF_HI(sp)                     
 		lw		v1,TF_EPC(sp)                    
-		mthi	v0                               
-		mtc0	v1,CP0_EPC                       
+		mthi	v0              //把v1寄存器中的内容赋值到hi寄存器                 
+		mtc0	v1,CP0_EPC      //把v1通用寄存器中的内容传输给EPC寄存器（存放EJTAG调试异常时最近的程序计数器值）                 
 		lw	x31,TF_REG31(sp)                 
 		lw	x30,TF_REG30(sp)                 
 		lw	x28,TF_REG28(sp)                 
@@ -173,7 +180,7 @@
 
 
 .macro SAVE_TF
-	mfc0	k0,CP0_STATUS                   
+	mfc0	k0,CP0_STATUS	//把处理器状态与控制寄存器中的内容赋给k0通用寄存器                  
 		sll		k0,3      /* extract cu0 bit */  
 		bltz	k0,1f                            
 		nop       
@@ -190,17 +197,17 @@
 		sub		sp,k1,TF_SIZE                    
 		sw		k0,TF_REG29(sp)                  
 		sw		x2,TF_REG2(sp)                   
-		mfc0	v0,CP0_STATUS                    
+		mfc0	v0,CP0_STATUS	//将处理器状态和控制寄存器中的内容赋给v0通用寄存器                    
 		sw		v0,TF_STATUS(sp)                 
-		mfc0	v0,CP0_CAUSE                     
+		mfc0	v0,CP0_CAUSE	//将最近异常原因寄存器中的内容赋给v0通用寄存器                     
 		sw		v0,TF_CAUSE(sp)                  
-		mfc0	v0,CP0_EPC                       
+		mfc0	v0,CP0_EPC      //将EPC寄存器中的内容赋给v0通用寄存器                 
 		sw		v0,TF_EPC(sp)
-		mfc0	v0, CP0_BADVADDR        
+		mfc0	v0, CP0_BADVADDR	//将最近异常地址寄存器中的内容赋给v0通用寄存器	        
 		sw		v0, TF_BADVADDR(sp)            
-		mfhi	v0                               
+		mfhi	v0              //把hi寄存器中的内容存入v0寄存器                 
 		sw		v0,TF_HI(sp)                     
-		mflo	v0                               
+		mflo	v0              //把lo寄存器中的内容存入v0寄存器                 
 		sw		v0,TF_LO(sp)                     
 		sw		x0,TF_REG0(sp)
 		sw		x1,TF_REG1(sp)                    
